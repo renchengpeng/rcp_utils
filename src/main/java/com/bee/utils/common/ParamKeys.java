564e3c91fd3c40503ef9c5e864a6c70fdc361552
xiongyindong <xiongyindong@beecredit.com>
2018-08-10 15:50:23 +0800

    添加个人异常

--- src/main/java/com/bee/utils/common/ParamKeys.java
+++ src/main/java/com/bee/utils/common/ParamKeys.java
@@ -310,6 +310,7 @@ public interface ParamKeys {
     public static final String WORKFLOW_DESIGN_SEQ_NO="WorkflowDesignService";
     public static final String GOODS_STORE_HIS_SEQ_NO="GoodsStoreHisService";
     public static final String GOODS_EXCEPTION_INFO_SEQ_NO="GoodsExceptionInfoService";
+    public static final String PERSONAL_EXCEPTION_INFO_SEQ_NO="PersonalExceptionInfoService";//20180810 xyd
     public static final String CUPBOARD_OPEN_HIS_SEQ_NO="CupboardOpenHisService";
     public static final String DELAY_USE_APPLY_SEQ_NO="DelayUseApplyService";
     public static final String CABINET_DOOR_INFO_SEQ_NO="CabinetDoorInfoService";
