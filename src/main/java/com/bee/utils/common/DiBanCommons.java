93b8be0257ea915e2957cc3fecb92f3386899793
wangjiucheng <wangjiucheng@beecredit.com>
2018-09-03 15:34:58 +0800

    添加解冻状态

--- src/main/java/com/bee/utils/common/DiBanCommons.java
+++ src/main/java/com/bee/utils/common/DiBanCommons.java
@@ -6,5 +6,14 @@ package com.bee.utils.common;
  * @author wangjiucheng
  */
 public class DiBanCommons {
-
+    /*** 请求后台路由前缀 */
+    public final static String VDB_ADMIN = "vdb";
+    
+    
+    /***********************税区状态 开始******************************/
+    //解冻状态
+    public final static String UNFREEZE="0";
+    //冻结状态
+    public final static String  FREEZE="1";
+    /***********************税区状态 结束******************************/
 }
