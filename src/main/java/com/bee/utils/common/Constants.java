adb86e7c1ea8c519261f0f1b7c425274662a2452
caowensheng <caowensheng@beecredit.com>
2018-09-21 15:30:17 +0800

    补充了点注释

--- src/main/java/com/bee/utils/common/Constants.java
+++ src/main/java/com/bee/utils/common/Constants.java
@@ -2548,10 +2548,11 @@ public interface Constants {
     /** 商城购物车单个商品最大数量 **/
     public static final Integer SHOPPING_CAR_PRODUCT_MAX_COUNT = 999;
     
-    /** 福员外科目状态 1:上架 **/
+    /** 福员外科目状态,商户科目关联关系状态  1:上架 **/
     public static final String FYW_INVOICEITEM_STATUS_ON = "1";
-    /** 福员外科目状态  2:下架 **/
+    /** 福员外科目状态,商户科目关联关系状态   2:下架 **/
     public static final String FYW_INVOICEITEM_STATUS_OFF = "2";
-
+    
+    
     
 }
