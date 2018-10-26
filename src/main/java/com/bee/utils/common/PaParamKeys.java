9bfb6830539cf9b6bbe3b23f9f59e750b87b7e5c
wangjiucheng <wangjiucheng@beecredit.com>
2018-10-26 17:47:51 +0800

    企业代付配置文件

---
+++ src/main/java/com/bee/utils/common/PaParamKeys.java
@@ -0,0 +1,15 @@
+
+package com.bee.utils.common;
+
+/**
+ * 卡里生成规则服务名称key
+ * 
+ * @author wangjiucheng
+ */
+public class PaParamKeys {
+
+    // 商户编号生成规则
+    //public static final String KALI_MERCHANT_NO = "merchantNoService";
+
+    
+}
