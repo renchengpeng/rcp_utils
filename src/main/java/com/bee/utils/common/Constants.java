48269bd78d62d6ec6373daffbf171b7211e882a2
caowensheng <caowensheng@beecredit.com>
2018-11-21 17:11:31 +0800

    添加了渠道标号

--- src/main/java/com/bee/utils/common/Constants.java
+++ src/main/java/com/bee/utils/common/Constants.java
@@ -458,6 +458,8 @@ public interface Constants {
     public final static String CHANNEL_17="17";
     /**快寄卖 **/
     public final static String CHANNEL_18="18";
+    /**消费券的员工商城 **/
+    public final static String CHANNEL_19="19";
     /**新商城（替换原有商城内划算/蜂贝）**/
     public final static String CHANNEL_20="20";
     /**消费券的二手商城 **/
