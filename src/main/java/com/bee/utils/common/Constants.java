2482e421c5275e97eb46c0b72f0bc41e7ec64648
fengjianjun <fengjianjun@beecredit.com>
2018-12-25 18:19:43 +0800

    wifi常量  视频常量

--- src/main/java/com/bee/utils/common/Constants.java
+++ src/main/java/com/bee/utils/common/Constants.java
@@ -1834,6 +1834,10 @@ public interface Constants {
     public static final String MOYAO_TRANSCODE_10019 = "10019";
     /** 10013-魔钥指令-发布视频 */
     public static final String MOYAO_TRANSCODE_10020 = "10020";
+    /** 10022-wifi信息  客户端向moyao推送连接wifi成功消息*/
+    public static final String MOYAO_TRANSCODE_10022 = "10022";
+    /**10023 客户端视频上传成功后，像后台**/
+    public static final String MOYAO_TRANSCODE_10023 = "10023";
     /** T0001-魔钥交易类型-指令开门 */
     public static final String MOYAO_TRANTYPE_T0001 = "T0001";
     /** T0002-魔钥交易类型-盘点标签 */
