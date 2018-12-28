efcf27dba8e5a6c43718928d3bf75024040df309
xuxitan <xuxitan@beecredit.com>
2018-12-28 13:17:21 +0800

    添加渠道

--- src/main/java/com/bee/utils/common/Constants.java
+++ src/main/java/com/bee/utils/common/Constants.java
@@ -491,8 +491,11 @@ public interface Constants {
     public final static String CHANNEL_21="21";
     /**消费券跃程充值 **/
     public final static String CHANNEL_22="22";
-    //蜂点企业
+    /**蜂点企业*/
     public final static String CHANNEL_23="23";
+    /** odm蜂点企业 */
+    public final static String CHANNEL_24="24";
+
     public final static String CHANNEL = "channel"; // by cws channel-web
 
     public final static String ADMIN = "admin";
